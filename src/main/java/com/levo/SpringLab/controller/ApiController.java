e9a8d1be52c354c8507a9be67f921ca32e51a32b
Lekeit00 <Lekeit00@hs-esslingen.de>
2022-04-11 12:06:58 +0200

    Lab 2 Done

--- src/main/java/com/levo/SpringLab/controller/ApiController.java
+++ src/main/java/com/levo/SpringLab/controller/ApiController.java
@@ -1,17 +1,90 @@
 package com.levo.SpringLab.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.levo.SpringLab.model.Task;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 
 @RestController
 public class ApiController {
+
+    ArrayList<Task> TaskList = new ArrayList<>();
 
     @GetMapping("/api/employees/{id}")
     @ResponseBody
     public String getEmployeesById(@PathVariable String id) {
         return "ID:" + id;
     }
+
+    @GetMapping("/api/tasks/")
+    public ArrayList<Task> getTasks() {
+        return TaskList;
+    }
+
+    @GetMapping("/api/tasks/{id}")
+    @ResponseBody
+    public Task getTaskByID(@PathVariable int id) {
+        for(Task task: TaskList) {
+            if(task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/api/task/add")
+    public Task addTask(HttpServletRequest request) {
+
+        String content = request.getParameter("content");
+        String title = request.getParameter("title");
+        Task task = new Task();
+        task.setId(TaskList.size() + 1);
+        task.setTitle(title);
+        task.setContent(content);
+        task.setDate(new Date());
+
+        TaskList.add(task);
+        return task;
+
+    }
+
+    @DeleteMapping("api/task/delete/{id}")
+   public Task removeTask(@PathVariable int id) {
+
+        for(Task task : TaskList) {
+            if(task.getId() == id) {
+                TaskList.remove(task);
+                return task;
+            }
+        }
+            return null;
+    }
+
+    @PutMapping("api/task/update/{id}")
+    public Task updateTask(@PathVariable int id, HttpServletRequest request) {
+
+        for(Task task : TaskList) {
+            if(task.getId()==id) {
+                if(request.getParameter("title") != null) {
+                    String title = request.getParameter("title");
+                    task.setTitle(title);
+                }
+                if(request.getParameter("content") != null) {
+                    String content = request.getParameter("content");
+                    task.setContent(content);
+                }
+                return task;
+            }
+
+
+        }
+        return null;
+    }
+
 
 }
---
+++ src/main/java/com/levo/SpringLab/model/Task.java
@@ -0,0 +1,43 @@
+package com.levo.SpringLab.model;
+
+import java.util.Date;
+
+public class Task {
+
+    Integer id;
+    String title;
+    String content;
+    Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
